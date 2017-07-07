0d95bcb40379a45f3fa4f7ea75c1ef58beb603a7
yulapshun <yulapshun@gmail.com>
2017-07-08 01:13:17 +0800

    Fix API request

--- web/app/js/index.js
+++ web/app/js/index.js
@@ -98,7 +98,7 @@ function updateChart(id) {
     var pageSize = PAGE_SIZE;
     var ctx = document.getElementById('chart');
     ctx.style.display = 'none';
-    axios.get('/api/get2?id=' + id + '&hour=' + 24).then(function(res) {
+    axios.get('/api/get?id=' + id + '&hour=' + 24).then(function(res) {
         ctx.style.display = 'block';
 
         let data = res.data.reverse();
